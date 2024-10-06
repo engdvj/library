f3324dfea4c79e9ae78897c6c0a5c8a9fc4dde80
engdvj <eng.dvj@gmail.com>
2024-10-06 16:11:54 -0300

    model creation

--- library/models.py
+++ library/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, blank=False, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Author(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    birth_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, blank=False)
+    description = models.TextField(blank=True, default='')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books')
+    publication_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
